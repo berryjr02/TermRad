c81610522823f0d02424fa7d44a577911d460652
berryjr02 <berryjr@umich.edu>
2026-03-27 13:36:40 -0400

    Gave forecast page bespoke screen
    TermRad loads as blank page, press 'f' to display forecast page. Screens should probably be implemented this way from now on.

--- TermRad.py
+++ TermRad.py
@@ -1,5 +1,6 @@
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Button, Label, Static
+from textual.screen import Screen
 from textual.containers import Container, ScrollableContainer
 from weather_api import get_numerical_forecast
 
@@ -12,37 +13,34 @@ class ForecastWidget(Static):
         p = self.period
         return f"[bold]{p['time']}[/bold]\n{p['temp']}°{p['unit']}\n{p['short_forecast']}\nWind: {p['wind']}\nPrecip: {p['precip']}"
 
+class TermRad(Screen):
+    BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
 
-class TermRad(App):
-    CSS_PATH = "TermRadStyles.tcss"
-
-    
-    # Define the widgets that compose the UI
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
         yield ScrollableContainer(id="forecast_container")
         yield Footer()
 
     def on_mount(self) -> None:
-        #hardcoded to flint for the sake of testing, this needs to be changed
         forecast_data = get_numerical_forecast(43.0125, -83.6875)
-            
-        #dynamically creates a widget for each "item" of forecast data that we retrieve
+
         forecast_widgets = [
-            ForecastWidget(period, id=f"forecast_{i}", classes="forecast_item")
-            for i, period in enumerate(forecast_data)
+                ForecastWidget(period, id=f"forecast{i}", classes="forecast_item")
+                for i, period in enumerate(forecast_data)
         ]
-            
 
         container = self.query_one("#forecast_container", ScrollableContainer)
-        #container = self.query_one("#forecast_container", Static)
         container.mount(*forecast_widgets)
-
+    
+class TermRad(App):
+    CSS_PATH = "TermRadStyles.tcss"
+    SCREENS = {"termrad": TermRad}
+    
     # async def on_key(self) -> None:
     #     await self.mount(Welcome())
     #     self.query_one(Button).label = "YES!"
 
-    BINDINGS = [("ctrl+q", "quit", "Quit"), ("h", "home", "Home"), ("r", "radar", "Radar"), ("f", "forecast", "Forecast"), ("s", "settings", "Settings"), ("ctrl+s", "screenshot", "Screenshot"), ("ctrl+a", "maximize", "Maximize")]
+    BINDINGS = [("ctrl+q", "quit", "Quit"), ("h", "home", "Home"), ("r", "radar", "Radar"), ("f", "push_screen('termrad')", "Forecast"), ("s", "settings", "Settings"), ("ctrl+s", "screenshot", "Screenshot"), ("ctrl+a", "maximize", "Maximize")]
 
     def action_quit(self) -> None:
         self.exit()
